d5bef7326b93668f7f1199fb17950d8d07c0deec
89068333383 <89068333383@mail.ru>
2026-07-20 20:19:53 +0500

    новые изменения

--- 03_lesson/smartphone.py
+++ 03_lesson/smartphone.py
@@ -3,9 +3,3 @@ class Smartphone:
         self.brand = brand
         self.model = model
         self.number = number
-
-
-fhone = Smartphone("Бабушка старушка", "Тряпочная", "+79000000000")
-print(fhone.brand)
-print(fhone.model)
-print(fhone.number)
